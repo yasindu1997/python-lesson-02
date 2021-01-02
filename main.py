19ad100aa10c56f8fcc0f17cd2988c0893056052
yasindu1997 <yasindusathsaraoth@gmail.com>
2021-01-02 09:47:14 +0530

    get name and nic using req parameters

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    name = f"{request.args['name']}"
+    nic = f"{request.args['nic']}"
+
+    print(f"Name is {name}")
+    print(f"NIC is {nic}")
+
+    return f"{name} <br> {nic}"
 
 
 if __name__ == '__main__':
