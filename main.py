35283e74d72d415a5bd8c5b48a66c30ef01c2d42
yasindu1997 <yasindusathsaraoth@gmail.com>
2021-01-02 10:16:37 +0530

    finished task of getting bday and gender

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from nic_parser.parser import Parser
 
 app = Flask(__name__)
 
@@ -8,11 +9,14 @@ app = Flask(__name__)
 def hello_world():
     name = f"{request.args['name']}"
     nic = f"{request.args['nic']}"
+    nic_pass = Parser(f"{request.args['nic']}")
+    bday = nic_pass.birth_date
+    gender = nic_pass.gender
 
     print(f"Name is {name}")
     print(f"NIC is {nic}")
 
-    return f"{name} <br> {nic}"
+    return f"{name} <br> {nic} <br> {bday} <br> {gender}"
 
 
 if __name__ == '__main__':
